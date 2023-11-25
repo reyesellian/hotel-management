a085a5a9829361a7f2e3fb996ec4c1ebb5af57f1
GabrielNarvadez <gab.narvadez@gmail.com>
2023-11-25 20:13:53 +0800

    Final

--- main/serializers.py
+++ main/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+import serializers
 from .models import Amenitie, Room, StandardRoom, FamilyRoom, DeluxeRoom
 
 class RoomSerializer(serializers.ModelSerializer):
